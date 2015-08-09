bdaf52ce6742abfda68175fb0b11edc76ac14cdb
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-08-09 19:36:01 +0200

    have a map rotation

--- load.d/1000-ssllite.lua
+++ load.d/1000-ssllite.lua
@@ -36,6 +36,40 @@ cs.ctftkpenalty = 0
 cs.lockmaprotation = 2
 cs.restrictpausegame = 0
 
+cs.maprotationreset()
+--copied from data/menus.cfg
+local ffamaps, capturemaps, ctfmaps = table.concat({
+  "aard3c academy akaritori alithia alloy aqueducts arbana bvdm_01 castle_trap collusion complex corruption curvedm curvy_castle darkdeath deathtek depot",
+  "dirtndust DM_BS1 dock douze duel7 duel8 dune elegy fanatic_quake force fragplaza frostbyte frozen fury guacamole gubo hades",
+"hashi hog2 industry injustice island justice kalking1 katrez_d kffa killfactory kmap5 konkuri-to ksauer1 legazzo lostinspace masdm mbt10",
+  "mbt2 mbt9 memento metl2 metl3 metl4 moonlite neondevastation neonpanic nmp8 nucleus oasis oddworld ogrosupply orbe orion osiris",
+  "ot outpost paradigm park pgdm phosgene pitch_black powerplant refuge renegade rm5 roughinery ruby ruine sauerstruck sdm1 shadowed",
+  "shindou shinmei1 shiva simplicity skrdm1 stemple suburb tartech teahupoo tejen thetowers thor torment tumwalk turbine wake5 wdcd"
+}, " "), table.concat({
+  "abbey akroseum alithia arabic asgard asteroids c_egypt c_valley campo capture_night caribbean collusion core_refuge core_transfer corruption cwcastle damnation",
+  "dirtndust donya duomo dust2 eternal_valley evilness face-capture fb_capture fc3 fc4 fc5 forge frostbyte hades hallo haste hidden",
+  "infamy killcore3 kopenhagen lostinspace mbt12 mercury monastery nevil_c nitro nmp4 nmp8 nmp9 nucleus ogrosupply paradigm ph-capture reissen",
+  "relic river_c serenity snapper_rocks spcr subterra suburb tempest tortuga turbulence twinforts urban_c valhalla venice xenon"
+}, " "), table.concat({
+  "abbey akroseum arbana asgard authentic autumn bad_moon berlin_wall bt_falls campo capture_night catch22 core_refuge core_transfer damnation desecration dust2",
+  "eternal_valley europium evilness face-capture flagstone forge forgotten garden hallo haste hidden infamy kopenhagen l_ctf mach2 mbt1 mbt12",
+  "mbt4 mercury mill nitro nucleus recovery redemption reissen sacrifice shipwreck siberia snapper_rocks spcr subterra suburb tejen tempest",
+  "tortuga turbulence twinforts urban_c valhalla wdcd xenon"
+}, " ")
+
+ffamaps, capturemaps, ctfmaps = map.uv(function(maps)
+  local t = map.f(L"_", maps:gmatch("[^ ]+"))
+  for i = 2, #t do
+    local j = math.random(i)
+    local s = t[j]
+    t[j] = t[i]
+    t[i] = s
+  end
+  return table.concat(t, " ")
+end, ffamaps, capturemaps, ctfmaps)
+
+cs.maprotation("ffa effic tac teamplay efficteam tacteam", ffamaps, "regencapture capture hold effichold instahold", capturemaps, "ctf efficctf instactf protect efficprotect instaprotect collect efficcollect instacollect", ctfmaps)
+
 cs.adduser("pisto", "SSL-admin", "+f198cd6e656129b898b7bb8d794211bd768aae54717d57df", "a")
 cs.adduser("Frosty", "SSL-admin", "-d1d22314f8dd21a1e038833cdd74feaf020b8aa7af534725", "a")
 cs.adduser("swatllama", "SSL-admin", "+d4e443aedd4dc3053f9a8de4769890d24361ebfea9a89046", "a")
@@ -85,6 +119,11 @@ require"std.getip"
 local maploaded, clanwar, tie, delayresume, chatisolate, specall, autospawn = require"std.maploaded", require"std.clanwar", require"gamemods.tie", require"std.delayresume", require"std.chatisolate", require"std.specall", require"gamemods.autospawn"
 
 local match, manualtie, manualautospawn
+
+spaghetti.addhook(server.N_MAPVOTE, function(info)
+  if info.skip or info.ci.privilege >= server.PRIV_ADMIN or not match then return end
+  info.skip = true
+end)
 
 spaghetti.addhook("changemap", function()
   if not match then return end
